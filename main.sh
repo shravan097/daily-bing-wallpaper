548fa8f4a65f3a4f1f677b6ee43bdc113911f833
Shravan Dhakal <shravan097@users.noreply.github.com>
2021-02-23 09:10:50 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+echo "Daily Bing Wallpaper Cron Running..."
+export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 cd ~/Pictures
 FILE_NAME='cron_bing_wallpaper.jpeg'
 BING_BASE_URL="https://bing.com"
+echo "Starting the download ...."
 IMAGE_URL=$(curl "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US" | jq -r '.images[0].url')
 echo ${BING_BASE_URL}${IMAGE_URL}
 wget -O "${FILE_NAME}" "${BING_BASE_URL}${IMAGE_URL}"
+echo "success"
