c9f2c2e29eeed3e6c49c2ad32bfc66d92ff5ce88
shravan097 <shravan097@users.noreply.github.com>
2021-03-07 00:39:05 -0800

    more stable

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Enlighten your mood by changing your wallaper automatically. A simple shell scri
 ### MacOS
 
 - Install `jq` & `wget`. You can use Homebrew `brew install jq` & `brew install wget`
-- Change wallpaper setting to use Picture folder and set Change Picture: When waking up from sleep
+- Run the script once to set download permanent picture. Mac looses file pointer and will not change picture accordingly if you skip this.
+- Run the script by doing `./main.sh` in your terminal.
+- Change wallpaper setting to use Picture folder and select `bing-wallpapers/permanent.jpeg` picture. set Change Picture: Every 1 minute. **Make sure to check Change Picture box.**
 - Give crontab [full file access](https://blog.bejarano.io/fixing-cron-jobs-in-mojave/). **Be careful and never to let other run cronjob**
 - `git clone` this repo 
 - Add crontab as the following
@@ -17,7 +19,11 @@ crontab -e
 0 */1 * * *  YOUR_SCRIPT_main_sh_FULL_PATH >> /tmp/cron.out 2>&1
 ```
 
+### Changing Number of Pictures to Save
+- Go to main.sh
+- Edit the variable `NUM_OF_PICS`
+
 ### TODO
-- size cli option
-- error handling
-- number of history images
+
+- Break into functions
+- Shell cli options
--- main.sh
+++ main.sh
@@ -2,10 +2,23 @@
 echo "Daily Bing Wallpaper Cron Running..."
 export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 cd ~/Pictures
-FILE_NAME='cron_bing_wallpaper.jpeg'
+mkdir -p bing-wallpapers
+cd bing-wallpapers
+NUM_OF_PICS='5'
+PERMANENT_FILE_NAME='permanent.jpeg'
 BING_BASE_URL="https://bing.com"
-echo "Starting the download ...."
-IMAGE_URL=$(curl "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US" | jq -r '.images[0].url')
-echo ${BING_BASE_URL}${IMAGE_URL}
-wget -O "${FILE_NAME}" "${BING_BASE_URL}${IMAGE_URL}"
-echo "success"
+RESPONSE_JSON=`curl "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=$NUM_OF_PICS&mkt=en-US"`
+if [ ! -f ${PERMANENT_FILE_NAME} ]; then
+echo "Setting first photo to be permanent...."
+IMAGE_URL=$(echo $RESPONSE_JSON | jq -r ".images[0].url")
+wget -O ${PERMANENT_FILE_NAME} "${BING_BASE_URL}${IMAGE_URL}"
+fi
+for ((i = 0; i <= $NUM_OF_PICS-1; i++))
+do
+    echo "Starting the download ....${i}"
+    IMAGE_URL=$(echo $RESPONSE_JSON | jq -r ".images[$i | tonumber].url")
+    echo ${BING_BASE_URL}${IMAGE_URL}
+    wget -O "${i}.jpeg" "${BING_BASE_URL}${IMAGE_URL}"
+    echo "success"
+done
+
